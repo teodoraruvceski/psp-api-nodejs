732443d2d5a638241ac1d0fe11674c8ed6351f57
teodoraruvceski <teodoraruvceski@gmail.com>
2023-01-05 17:14:09 +0100

    5.1.

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const { createLogger, format, transports } = require("winston");
 //database pw:12345678910nebojsa
 const supabaseUrl = 'https://bxrtifkbinmnalcnwjyo.supabase.co'
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4cnRpZmtiaW5tbmFsY253anlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzIzNDU3MTUsImV4cCI6MTk4NzkyMTcxNX0.2EViTiVuraLljRLmjpJSlroYagDVVj0x6_rYLANy638"
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const merchantBankId='69b07549-69a7-473e-8bbc-44440fcb1280';
+const merchanBankPassword='123123';
+const merchantBankUrl='http://localhost:8000';
+const pspSuccessUrl='http://localhost:3001/success';
+const pspFailedUrl='http://localhost:3001/error';
+const pspErrorUrl='http://localhost:3001/error';
+
 const logLevels = {
   fatal: 0,
   error: 1,
@@ -25,7 +33,7 @@ const logger = createLogger({
   exceptionHandlers: [new transports.File({ filename: "exceptions.log" })],
   rejectionHandlers: [new transports.File({ filename: "rejections.log" })],
 });
-app.listen(5000, () => console.log(`Server Started on ${5000}`));
+app.listen(5000, () => console.log(`Server Started on ${5000} `));
 
 app.get('/', (req, res) => 
 {
@@ -35,9 +43,9 @@ app.get('/', (req, res) =>
 });
 app.get('/agency-url-success-registration', async(req, res) => 
 {
-    console.log("getPspUrl");
+    console.log("agency-url-success-payment");
     try{
-      const response = await axios.get(`http://localhost:6001/agency-url-success-registration`);
+      const response = await axios.get(`http://localhost:6001/agency-url-success-payment`);
       console.log("resp:",response.data);
       logger.info(`from:${req.url}. sending response: ${response.data}`);
       res.send(response.data);
@@ -56,22 +64,51 @@ app.get('/get-psp-url', (req, res) =>
 });
 
 //??prvi poziv za placanje sa fronta psp kada biramo nacin placanja
+app.post('/payByCard',async(req,res)=>
+{
+  console.log(req.query);
+  const paymentId=req.query.paymentId;
+  const total=req.query.total;
+  console.log(total);
+  const paymentInfo={
+    merchant_id:merchantBankId,
+    merchant_password:merchanBankPassword,
+    amount:total,
+    merchant_order_id:paymentId,
+    merchant_timestamp:Date.now(),
+    success_url:pspSuccessUrl,
+    failed_url:pspFailedUrl,
+    error_url:pspErrorUrl
+  };
+  console.log('total');
+  try{
+
+    const data=await axios.post('http://localhost:8000/start-payment',paymentInfo);
+    console.log(data);
+    res.send(data.data+`?paymentdId=${paymentId}`); //sending bank-front url to psp-front
+  }
+  catch(e)
+  {
+    console.log(e);
+  }
+});
 app.post('/payRegistration', async (req, res) => {
   console.log("payRegistration");
     const total=req.query.total;
     const paymentId=req.query.paymentId;
+    //send paypal merchant account info (id, password) // dont store it on payPal api   !!!! 
     try{
       const response= await axios.post(`http://localhost:3005/payRegistration?total=${total}&paymentId=${paymentId}`);
       console.log("resp:",response.data);
       res.send(response.data);
       //dodao za supabase da upisuje
-      const {data}= supabase
-      .from('companies')
-      .insert([
-        {total,payment_id}
-       ])
-    .single();
-    res.send(id);
+    //   const {data}= supabase
+    //   .from('companies')
+    //   .insert([
+    //     {total,paymentId}
+    //    ])
+    // .single();
+    //res.send(id);
     }
     catch(e){
       console.log("error:"+e);
